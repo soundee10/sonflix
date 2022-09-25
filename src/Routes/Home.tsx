2eb8e09ee821e04df714153dae05647545500f6b
unknown <malice@korea.ac.kr>
2022-09-25 23:55:12 +0900

    1.8 Box animation

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { makeImagePath } from "../utils";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import { useHistory, useRouteMatch } from "react-router-dom";
 
 const Wrapper = styled.div`
     background: black;
@@ -33,10 +34,30 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
  background-image: url(${(props)=>props.bgPhoto});
  background-size:cover;
  background-position: center center;
- height: 300px;
+ height: 200px;
  font-size: 50px;
- color: tomato;
+ cursor: pointer;
+  &:first-child{
+    transform-origin: center left;
+ }
+ &:last-child{
+    transform-origin: center right;
+ }
 `;
+
+const boxVariants = {
+    normal:{
+        scale:1
+    },
+    hover:{
+        scale:1.3,
+        y: -50,
+        transition: {
+            delay: 0.3,
+            duration: 0.3
+        }
+    }
+}
 
 const Banner = styled.div <{ bgPhoto: string }>`
  height: 100vh;
@@ -58,6 +79,25 @@ const Overview = styled.p`
  width: 50%;
 `;
 
+const Info = styled(motion.div)`
+ padding: 10px;
+ background-color: ${(props)=>props.theme.black.lighter};
+ opacity: 0;
+ position: absolute;
+ width: 100%;
+ bottom: 0;
+ h4{
+    text-align: center;
+    font-size:18px;
+ }
+`;
+
+const infoVariants ={
+    hover:{
+        opacity:1,
+    }
+}
+
 const rowVariants = {
     hidden: {
         x: window.outerWidth+5,
@@ -71,6 +111,9 @@ const rowVariants = {
 }
 
 function Home() {
+    const history = useHistory()
+    const bigMovieMatch = useRouteMatch<{movieId:string}>("/movies/:movieId")
+    console.log(bigMovieMatch);
     const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies)
     const [index, setIndex] = useState(0);
     const [leaving, setLeaving] = useState(false);
@@ -83,6 +126,9 @@ function Home() {
             const maxIndex = Math.ceil(totalMovies / offset);
             setIndex((prev)=>(prev===maxIndex) ? 0 : prev+1);
         }
+    };
+    const onBoxClicked = (movieId:number) =>{
+        history.push(`/movies/${movieId}`)
     };
     const toggleLeaving = () =>{
         setLeaving((prev)=>!prev);
@@ -99,14 +145,37 @@ function Home() {
             <Slider>
                 <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
                     <Row variants={rowVariants} initial="hidden" animate = "visible"
-                         exit="exit" transition={{type:"tween", duration: 3}} key={index}>
+                         exit="exit" transition={{type:"tween", duration: 2}} key={index}>
                         {data?.results.slice(1).slice(offset * index,offset*index+offset)
                         .map((movie)=>(
-                            <Box key={movie.id} bgPhoto={makeImagePath(movie.backdrop_path, "w500")}/>
+                            <Box key={movie.id} whileHover="hover" initial="normal" transition={{type:"tween"}}
+                            variants={boxVariants} onClick={()=>onBoxClicked(movie.id)}
+                            bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                            layoutId={movie.id+""}
+                            >
+                            <Info variants={infoVariants}>
+                                <h4>{movie.title}</h4>
+                            </Info>
+                            </Box>
                         ))}
                     </Row>
                 </AnimatePresence>
             </Slider>
+                {bigMovieMatch ? <AnimatePresence>
+                    <motion.div
+                    layoutId={bigMovieMatch.params.movieId}
+                    style={{
+                        position: "absolute",
+                        height: "40vh",
+                        width: "35vw",
+                        backgroundColor: "white",
+                        top: 50,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        margin: "auto"
+                    }}/>
+                </AnimatePresence> : null}
         </>
     )}</Wrapper>;
 
